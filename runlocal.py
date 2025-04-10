8e0f0fd9021bbe3fde9d4b399af8e3ee6e3ea303
lorenzo <lorenzo@lorenzo.lorenzo>
2025-04-10 19:59:49 +0200

    added runlocal.py

---
+++ runlocal.py
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+def main():
+    import os
+    import sys
+    from pathlib import Path
+    from openpyxl import load_workbook
+    from io import BytesIO
+
+    # Get the current working directory
+    current_directory = Path(__file__).resolve().parent
+
+    # Add the parent directory to the system path
+    sys.path.append(str(current_directory.parent))
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionale.settings.local')
+    os.environ.setdefault('DEMO_PASSWORD', 'das801n3io298ad1nulad')
+    SEED_DATA = False
+
+    import django
+    django.setup()
+
+    from django.core.management import execute_from_command_line
+    from django.conf import settings
+    from django.core.management import call_command
+    from django.contrib.auth.models import User
+    from django.utils.crypto import get_random_string
+    from datetime import datetime
+    from django.http import JsonResponse
+    from api.models import Employee, Import, Role
+
+
+    if SEED_DATA:
+        call_command('makemigrations')  # Apply migrations
+        call_command('migrate')  # Apply migrations
+        print("Migrations applied successfully.")
+
+        call_command('seed')
+        print("Seed data loaded successfully.")
+
+    call_command('collectstatic', interactive=False)
+    print("Static files collected successfully.")
+
+
+
+    # Run the Django development server
+    call_command('runserver')
+
+
+if __name__ == "__main__":
+    main()
+    # main()
