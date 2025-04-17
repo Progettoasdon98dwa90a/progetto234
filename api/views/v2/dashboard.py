c07849b118b0284f58112d12899669e17c3acb70
lorenzo <lorenzo@lorenzo.lorenzo>
2025-04-17 19:39:59 +0200

    gethistory modification

--- api/views/v2/dashboard.py
+++ api/views/v2/dashboard.py
@@ -4,52 +4,51 @@ from api.models import Employee
 
 
 def dashboard_data(request, branch_id):
-    if request.method != 'GET':
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+    if request.method == 'GET':
 
-    data = {
-        'metrics' :{},
-        'topEmployees': [],
-        'pendingShifts': {},
-    }
-
-    metrics_data = {
-        'totalReceipts' : 0, # Last 7 Days
-        'incomes' : 0, # Current Month
-        'peopleCount' : 0, # Current Month
-        'monthlyTarget' : {
-            'monthlyBudget' : 0,
-            'actualIncome' : 0,
-            'reachedIncomePercentage' : 0,
-            'variation_percentage' : 0,
-
+        data = {
+            'metrics' :{},
+            'topEmployees': [],
+            'pendingShifts': {},
         }
-    }
 
-    data['metrics'] = metrics_data
+        metrics_data = {
+            'totalReceipts' : 0, # Last 7 Days
+            'incomes' : 0, # Current Month
+            'peopleCount' : 0, # Current Month
+            'monthlyTarget' : {
+                'monthlyBudget' : 0,
+                'actualIncome' : 0,
+                'reachedIncomePercentage' : 0,
+                'variation_percentage' : 0,
 
-    top_employees_data = []
+            }
+        }
 
-    employee_object = {
-        'name' : '',
-        'average_sold_pieces' : 0,
-        'mediumReceipts' : 0, # value
-        'averageReceipts' : 0,
-    }
+        data['metrics'] = metrics_data
 
-    employees = Employee.objects.filter(branch_id=branch_id)
+        top_employees_data = []
 
-    for employee in employees:
-        employee_object['name'] = employee.first_name + ' ' + employee.last_name
-        employee_object['average_sold_pieces'] = 0
-        employee_object['mediumReceipts'] = 0
-        employee_object['averageReceipts'] = 0
+        employee_object = {
+            'name' : '',
+            'average_sold_pieces' : 0,
+            'mediumReceipts' : 0, # value
+            'averageReceipts' : 0,
+        }
 
-        top_employees_data.append(employee_object)
+        employees = Employee.objects.filter(branch_id=branch_id)
 
-    data['topEmployees'] = top_employees_data
+        for employee in employees:
+            employee_object['name'] = employee.first_name + ' ' + employee.last_name
+            employee_object['average_sold_pieces'] = 0
+            employee_object['mediumReceipts'] = 0
+            employee_object['averageReceipts'] = 0
 
-    pending_shifts_data = []
+            top_employees_data.append(employee_object)
+
+        data['topEmployees'] = top_employees_data
+
+        pending_shifts_data = []
 
 
-    return data
+        return data
