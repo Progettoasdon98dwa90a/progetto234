f3a9554e39cccef8aa0486d66f1c1a25338520ad
lorenzo <lorenzo@lorenzo.lorenzo>
2025-04-25 17:00:22 +0200

    employee rest_days endpoint

--- api/views/v2/imports.py
+++ api/views/v2/imports.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
+from io import BytesIO
 
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from openpyxl.reader.excel import load_workbook
 
-from api.models import Branch, Import
+from api.models import Branch, Import, Employee
 
 
 def getHistory(request, year, branch_id):
@@ -74,10 +76,259 @@ def getHistory(request, year, branch_id):
 @csrf_exempt
 def uploadImportData(request):
     if request.method == 'POST':
-        uploaded_file = request.FILES['file']
-        branch_id = request.POST.get('branch_id', -1)
-        print(uploaded_file, branch_id)
+        uploaded_file = request.FILES.get('file')
+        selected_branch = request.POST.get('branchSelect')
+        selected_type = request.POST.get('typeSelect')
 
-        return JsonResponse({'status': 'success', 'message': 'File uploaded successfully'})
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+        # 0 = sales_data, 1 = counter_data
+        if selected_type == 0:
+            selected_type = "sales_data"
+        elif selected_type == 1:
+            selected_type = "counter_data"
+
+        try:
+            selected_branch = int(selected_branch)
+        except:
+            return JsonResponse({"status": "error", "errors": ["Internal"]}, status=200)
+
+        if not selected_branch:
+            return JsonResponse({"status": "error", "errors": ["no branch selected"]}, status=200)
+
+        data_dict = dict
+
+        branch_obj = Branch.objects.get(id=selected_branch)
+
+        if branch_obj.get_brand() == "equivalenza":
+            if selected_type == "counter_data":
+                if uploaded_file:
+                    # Read the file as binary
+                    file_content = uploaded_file.read()
+
+                    # Load the workbook from the binary data
+                    workbook = load_workbook(filename=BytesIO(file_content))
+                    sheet = workbook.active  # You can specify sheet name if needed
+
+                    # Initialize the dictionary to hold the data
+                    data_dict = {}
+
+                    errors = []
+
+                    total_rows = sheet.max_row
+
+                    # Iterate through the rows, skipping the header
+                    for row in sheet.iter_rows(min_row=2, values_only=True):  # Skip the header row
+                        date = row[0]  # assuming the date is in the second column
+                        record = {
+                            "(Ing) Ingressi": row[1] or 0,
+                            "(Est) Traffico Esterno": row[2] or 0,
+                            "(TA) Tasso di Attrazione": row[3] or 0,
+                        }
+
+                        # mar-19.11.24 to YYYY/MM/DD
+                        date = date.split("-")[1]
+                        date = datetime.strptime(date, "%d.%m.%y").strftime("%Y-%m-%d")
+
+                        # If the date is already a key, overwrite the current record
+                        if date in data_dict:
+
+                            data_dict[date] = record
+                        else:
+                            # Otherwise, create a new list with the record
+                            data_dict[date] = [record]
+
+                        data_dict[date] = [record]
+
+                    for date, records in data_dict.items():
+                        if Import.objects.filter(branch=branch_obj, import_date=date,
+                                                 import_type=selected_type).exists():
+                            errors.append(f"Collision on {date}")
+                            continue
+
+                    if errors:
+                        return JsonResponse({"status": "error", "errors": errors}, status=200)
+
+                    import_bulk_create_list = []
+                    for date, data in data_dict.items():
+                        i = Import(import_date=date, data=data, branch=branch_obj, import_type=selected_type)
+                        import_bulk_create_list.append(i)
+
+                    Import.objects.bulk_create(import_bulk_create_list)
+
+                    return JsonResponse({"status": "success"}, status=200)
+
+            ### START CONVERTING DATA
+            elif selected_type == "sales_data":
+                if uploaded_file:
+                    # Read the file as binary
+                    file_content = uploaded_file.read()
+
+                    # Load the workbook from the binary data
+                    workbook = load_workbook(filename=BytesIO(file_content))
+                    sheet = workbook.active  # You can specify sheet name if needed
+
+                    # Initialize the dictionary to hold the data
+                    data_dict = {}
+
+                    # Iterate through the rows, skipping the header
+                    for row in sheet.iter_rows(min_row=2, values_only=True):  # Skip the header row
+                        date = row[1]  # assuming the date is in the second column
+                        record = {
+                            "Dipendente": row[0],
+                            "Qta. Vend.": row[2],
+                            "Sco.": row[3],
+                            "Importo": row[4],
+                            "Sco. Medio": row[5],
+                            "Qta Media": row[6]
+                        }
+
+                        # If the date is already a key, append the new record to its list
+                        if date in data_dict:
+                            data_dict[date].append(record)
+                        else:
+                            # Otherwise, create a new list with the record
+                            data_dict[date] = [record]
+
+                    import_qs = Import.objects.none()
+
+                    errors = []
+
+                    for date, records in data_dict.items():
+                        employees_day_id_list = []
+                        date = datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d')
+                        for record in records:
+                            try:
+                                employees_day_id_list.append(record['Dipendente'])
+                            except:
+                                errors.append(f"Employee {record['Dipendente']} not found")
+                                continue
+
+                        employees_day_qs = Employee.objects.filter(id__in=employees_day_id_list)
+
+                        if employees_day_qs.count() != len(employees_day_id_list):
+                            errors.append(f"Employees on {date} not found")
+                            continue
+
+                        branch_check_value = list(employees_day_qs.values_list('branch', flat=True))
+                        normalized = list(dict.fromkeys(branch_check_value))
+                        if len(normalized) != 1:
+                            errors.append(f"Branch on {date} from different branch")
+                            continue
+
+                        if normalized[0] != selected_branch:
+                            errors.append(f"Branch on {date} from different branch")
+                            continue
+
+                        if Import.objects.filter(branch=branch_obj, import_date=date):
+                            errors.append(f"Collision on {date}")
+                            continue
+
+                    if errors:
+                        return JsonResponse({"status": "error", "errors": errors}, status=200)
+
+                    import_bulk_create_list = []
+                    for date, data in data_dict.items():
+                        date_obj = datetime.strptime(date, '%d/%m/%Y')
+                        date_str = date_obj.strftime('%Y-%m-%d')
+                        i = Import(import_date=date_str, data=data, branch=branch_obj,
+                                   import_type=selected_type)
+                        import_bulk_create_list.append(i)
+
+                    Import.objects.bulk_create(import_bulk_create_list)
+
+                return JsonResponse({"status": "success", "errors": []}, status=200)
+
+        elif branch_obj.get_brand() == "original":
+            print("original")
+
+            if selected_type == "counter_data":
+                if uploaded_file:
+                    # Read the file as binary
+                    file_content = uploaded_file.read()
+
+                    # Load the workbook from the binary data
+                    workbook = load_workbook(filename=BytesIO(file_content))
+                    sheet = workbook.active  # You can specify sheet name if needed
+
+                    # Initialize the dictionary to hold the data
+                    data_dict = {}
+
+                    errors = []
+
+                    total_rows = sheet.max_row
+
+                    # Iterate through the rows, skipping the header
+                    for row in sheet.iter_rows(min_row=2, values_only=True):  # Skip the header row
+
+                        date = row[0]  # assuming the date is in the second column
+                        record = {
+                            "(Ing) Ingressi": row[1],
+                            "(Est) Traffico Esterno": row[2],
+                            "(TA) Tasso di Attrazione": row[3],
+                        }
+
+                        data_dict[date] = [record]
+
+            ### START CONVERTING DATA
+            elif selected_type == "sales_data":
+
+                if uploaded_file:
+                    # Read the file as binary
+                    file_content = uploaded_file.read()
+
+                    # Load the workbook from the binary data
+                    workbook = load_workbook(filename=BytesIO(file_content))
+                    sheet = workbook.active  # You can specify sheet name if needed
+
+                    # Initialize the dictionary to hold the data
+                    data_dict = {}
+
+                    errors = []
+
+                    total_rows = sheet.max_row
+
+                    # Iterate through the rows, skipping the header
+                    for row in sheet.iter_rows(min_row=2, values_only=True):  # Skip the header row
+
+                        if row[1] in ["Totale generale", "Totali "]:
+                            continue
+
+                        date = row[1]  # assuming the date is in the second column
+                        record = {
+                            "Qta. Vend.": row[2],
+                            "Sco.": row[3],
+                            "Importo": row[4],
+                            "Sco. Medio": row[5],
+                            "Qta Media": row[6]
+                        }
+                        if Import.objects.filter(branch=branch_obj, import_date=date):
+                            errors.append(f"Collision on {date}")
+                            continue
+
+                        data_dict[date] = [record]
+
+                    import_qs = Import.objects.none()
+
+                    if errors:
+                        return JsonResponse({"status": "error", "errors": errors}, status=200)
+
+                    data_dict_formatted = {}
+
+                    data_dict = {k: v for k, v in data_dict.items() if k is not None}
+
+                    for k, v in data_dict.items():
+                        unformatted = k
+                        date_strp = datetime.strptime(unformatted, '%d/%m/%Y')
+                        date_str = date_strp.strftime('%Y-%m-%d')
+                        data_dict_formatted[date_str] = v
+
+                    import_bulk_create_list = []
+                    for date, data in data_dict_formatted.items():
+                        i = Import(import_date=date, data=data, branch=branch_obj, import_type=selected_type)
+                        import_bulk_create_list.append(i)
+
+                    Import.objects.bulk_create(import_bulk_create_list)
+
+                return JsonResponse({"status": "success", "errors": []}, status=200)
+
+        return JsonResponse({"status": "error", "errors": ["Internal"]}, status=200)
+    return JsonResponse({"status": "error", "errors": ["Internal"]}, status=200)
