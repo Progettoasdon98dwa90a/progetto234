4e814e4523b55ab79f3d2f43c1157c4342943923
lorenzo <lorenzo@lorenzo.lorenzo>
2025-04-26 17:02:43 +0200

    report branch

--- api/urls.py
+++ api/urls.py
@@ -45,8 +45,11 @@ urlpatterns = [
     path('dipendenti/<int:branch_id>/', get_branch_employees, name='GET_BRANCH_EMPLOYEES'),
     path('dipendenti/<int:branch_id>/createEmployee/', new_employee, name='CREATE_EMPLOYEE'),
     path('dipendenti/<int:branch_id>/updateEmployee/<int:employee_id>/', update_employee, name='UPDATE_EMPLOYEE'),
-    path('<int:branch_id>/report/branch/', get_branch_report, name='GET_BRANCH_REPORT'),
     path('<int:branch_id>/report/employees/', get_employees_report, name='GET_EMPLOYEES_REPORT'),
+
+    path('report/<int:branch_id>/', get_branch_report, name='GET_BRANCH_REPORT'),
+
+
     path('dipendenti/<int:branch_id>/updateEmployeeRestDays/<int:employee_id>/', set_employee_rest_days, name='SET_EMPLOYEE_REST_DAYS'),
 
     path('turni/<int:branch_id>/', get_branch_schedules, name='GET_SCHEDULES'),
