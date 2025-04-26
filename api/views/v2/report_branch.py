b1a43c76c852db490c01312579d9cc715b2016c3
lorenzo <lorenzo@lorenzo.lorenzo>
2025-04-26 17:24:39 +0200

    report branch endpoint for single graphs

--- api/views/v2/report_branch.py
+++ api/views/v2/report_branch.py
@@ -17,8 +17,8 @@ from api.formulas.counter import generate_branch_tasso_attrazione_report
 def get_branch_report(request, branch_id):
     if request.method == 'GET':
         # Calculate fallback to the last 30 days
-        start_date = datetime.now() - timedelta(days=7)
-        end_date = datetime.now()
+        start_date = datetime.now() - timedelta(days=371) # 7 days
+        end_date = datetime.now()  - timedelta(days=365)
 
         start_date_str = start_date.strftime('%Y-%m-%d')
         end_date_str = end_date.strftime('%Y-%m-%d')
@@ -35,11 +35,9 @@ def get_branch_report(request, branch_id):
 
         report_data = {
             "sales": generate_branch_report_sales(branch_id, start_date_str, end_date_str),
-            "scontrini": generate_branch_report_scontrini(branch_id, start_date_str, end_date_str),
-            "ingressi": generate_ingressi_branch_report(branch_id, start_date_str, end_date_str),
-            "tasso_attrazione": generate_branch_tasso_attrazione_report(branch_id, start_date_str, end_date_str),
-            "conversion_rate": generate_branch_report_conversion_rate(branch_id, start_date_str, end_date_str),
-            "traffico_esterno": generate_branch_traffico_esterno_report(branch_id, start_date_str, end_date_str),
+            "receipts": generate_branch_report_scontrini(branch_id, start_date_str, end_date_str),
+            "entrances": generate_ingressi_branch_report(branch_id, start_date_str, end_date_str),
+            "conversionRate": generate_branch_report_conversion_rate(branch_id, start_date_str, end_date_str),
         }
 
         return JsonResponse({"status": "success", "data": report_data})
