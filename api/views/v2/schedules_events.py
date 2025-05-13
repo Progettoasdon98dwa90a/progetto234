36284abbab11697c4fd1d9acc018d2079f36bea3
lorenzo <lorenzo@lorenzo.lorenzo>
2025-05-13 11:54:41 +0200

    fixed cors

--- api/views/v2/schedules_events.py
+++ api/views/v2/schedules_events.py
@@ -6,9 +6,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def create_schedule_event(request, schedule_id, employee_id, event, start_time, end_time, color):
     if request.method != 'POST':
-        return JsonResponse("Invalid request method", status=405)
+        return JsonResponse("status" : "error", status=405)
     if not all([schedule_id, employee_id, event, start_time, end_time, color]):
-        return JsonResponse("Missing required parameters", status=400)
+        return JsonResponse("status" : "error", status=500)
     
     try:
         schedule_event = ScheduleEvent.objects.create(
@@ -20,23 +20,23 @@ def create_schedule_event(request, schedule_id, employee_id, event, start_time, 
         
         schedule_event.save()
         print(f"Schedule event created successfully: {schedule_event}")
-        return JsonResponse("Schedule event created successfully", status=201)
+        return JsonResponse({"status": "success"}, status=200)
     except Exception as e:
         print(f"Error creating schedule event: {e}")
         schedule_event = None
-        return JsonResponse("Error creating schedule event", status=500)
+        return JsonResponse("status" : "error", status=500)
     
 
 def get_schedule_events(request, schedule_id):
     if request.method == 'GET':
         try:
             schedule_events = ScheduleEvent.objects.filter(schedule_id=schedule_id)
-            return JsonResponse("Schedule events retrieved successfully", status=200)
+            return JsonResponse({"status": "success"}, status=200)
         except ScheduleEvent.DoesNotExist:
-            return JsonResponse("Schedule events not found", status=404)
+            return JsonResponse("status" : "error", status=404)
         except Exception as e:
             print(f"Error getting schedule events: {e}")
-            return JsonResponse("Error getting schedule events", status=500)
+            return JsonResponse("status" : "error", status=500)
     else:
-        return JsonResponse("Invalid request method", status=405)
+        return JsonResponse("status" : "error", status=405)
     
