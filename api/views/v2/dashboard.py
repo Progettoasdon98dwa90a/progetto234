c2eab9ed0a0e687cd327849aedd0a49228a0e6cd
lorenzo <lorenzo@lorenzo.lorenzo>
2025-05-17 19:10:00 +0200

    schedule creation with periods functions, not implements yet

--- api/views/v2/dashboard.py
+++ api/views/v2/dashboard.py
@@ -28,15 +28,13 @@ def dashboard_data(request, branch_id):
 
         people_count = get_number_ingressi_date_range(branch_id, last_month_start_date, current_date)
         current_target = Target.objects.filter(branch_id=branch_id, start_date=last_month_start_date).first()
+        sales_target = current_target.sales_target
         reached_income_percentage = 0
 
         # Calculate the percentage, ensuring the monthly budget is not zero to prevent division error
         if current_target.sales_target is not None and current_target.sales_target > 0:
-            # Perform the division and multiply by 100
-            reached_income_percentage = (float(total_sales) / current_target.sales_target) * 100
-            # Optional: Round the percentage to a reasonable number of decimal places
-            reached_income_percentage = round(reached_income_percentage, 2)  # Round to 2 decimal places
-            # Note: This will correctly show percentages > 100 if actual_income exceeds the target
+            reached_income_percentage = (float(total_sales) / float(sales_target)) * 100
+            reached_income_percentage = round(reached_income_percentage, 2)
         data = {
             'metrics' :{},
             'topEmployees': [],
